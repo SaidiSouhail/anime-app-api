10bed97808dd5d6af6f124a92e8619dedcd9295f
SaidiSouhail <tboss360@yahoo.fr>
2026-06-05 17:27:30 +0100

    fix: corrected vercel build folder issue

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
-import { SearchProvider } from "./context/SearchContext.jsx";
+import { SearchProvider } from "./Context/SearchContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
